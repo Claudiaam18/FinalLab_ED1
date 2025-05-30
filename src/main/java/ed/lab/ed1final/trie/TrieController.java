04f55c86ef02cc63c4a68c9f1e295fee0409255b
Claudiaam18 <158302256+Claudiaam18@users.noreply.github.com>
2025-05-30 07:49:51 -0600

    Ejercicios

--- src/main/java/ed/lab/ed1final/trie/TrieController.java
+++ src/main/java/ed/lab/ed1final/trie/TrieController.java
@@ -1,12 +1,27 @@
 package ed.lab.ed1final.trie;
-import java.util.*;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/trie")
 public class TrieController {
+
     private final Trie trie;
-    public TrieController(Trie trie){
+
+    public TrieController(Trie trie) {
         this.trie = trie;
     }
-    @PostMapping("/{word}")
-    public ResponseEntity<Void> insertword(@PathVariable)
-}
 
+    @PostMapping("/{word}")
+    public ResponseEntity<Void> insertWord(@PathVariable String word) {
+        if (!word.matches("[a-z]+") || word.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        trie.insert(word);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
